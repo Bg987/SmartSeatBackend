9253ae7eef2d14418be863c073020cb31c8ea567
Bg <119032764+Bg987@users.noreply.github.com>
2026-02-14 13:32:09 +0530

    add email service communication using kafka

---
+++ src/main/java/com/example/SmartSeatBackend/controller/UniversityController.java
@@ -0,0 +1,83 @@
+package com.example.SmartSeatBackend.controller;
+
+import com.example.SmartSeatBackend.DTO.SubjectDTO;
+import com.example.SmartSeatBackend.DTO.TempCollegeDTO;
+import com.example.SmartSeatBackend.entity.User;
+import com.example.SmartSeatBackend.service.UniversityService;
+import com.example.SmartSeatBackend.utility.StringProcess;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@AllArgsConstructor
+public class UniversityController {
+
+    private final UniversityService uniservice;
+
+
+    //only single college
+    @PreAuthorize("hasRole('university')")
+    @PostMapping("/addCollege")
+    public ResponseEntity<String> addCollege(@Valid @RequestBody TempCollegeDTO collageData)
+    {
+        try{
+            return  uniservice.addCollege(collageData);
+        }
+        catch(Exception e){
+            System.out.println("error in college insert "+e.getMessage());
+            return ResponseEntity.status(409).body("college already exist in database");
+        }
+    }
+
+    //using csv
+    @PreAuthorize("hasRole('university')")
+    @PostMapping("/addColleges")
+    public ResponseEntity<?> addColleges(@RequestParam("file") MultipartFile file) {
+        try {
+                List<String> responses = uniservice.saveCollegesFromCSV(file);
+
+                System.out.println(responses);
+                return ResponseEntity.ok(responses);
+
+
+            } catch (DataIntegrityViolationException ex) {
+
+                return ResponseEntity
+                        .status(HttpStatus.CONFLICT)
+                        .body(List.of("Duplicate Email Found: " +
+                                ex.getMostSpecificCause().getMessage()));
+
+            } catch (Exception e) {
+
+                return ResponseEntity
+                        .status(HttpStatus.BAD_REQUEST)
+                        .body(List.of("Error processing file: " + e.getMessage()));
+            }
+    }
+
+    @PreAuthorize("hasRole('university')")
+    @GetMapping("/colleges")
+    public ResponseEntity<List<User>> getAllColleges() {
+        return uniservice.getAllColleges();
+    }
+
+    @PreAuthorize("hasRole('university')")
+    @PostMapping("/addSubject")
+    public ResponseEntity addSubject(@RequestBody SubjectDTO subject){
+        return uniservice.addSubject(subject);
+    }
+}
+
+
+
+
