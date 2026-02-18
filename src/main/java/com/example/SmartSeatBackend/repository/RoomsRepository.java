aaaf0ab245bf9fdbb505aba39b107ec134e76c8b
Bg <119032764+Bg987@users.noreply.github.com>
2026-02-18 18:10:36 +0530

    conflict fixes

--- src/main/java/com/example/SmartSeatBackend/repository/RoomsRepository.java
+++ src/main/java/com/example/SmartSeatBackend/repository/RoomsRepository.java
@@ -3,11 +3,18 @@ package com.example.SmartSeatBackend.repository;
 import com.example.SmartSeatBackend.entity.College;
 import com.example.SmartSeatBackend.entity.Rooms;
 import com.example.SmartSeatBackend.entity.Students;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface RoomsRepository extends JpaRepository<Rooms,Integer> {
-    boolean existsByRoomNumberAndCollege(Integer roomNumber, College college);
+
+
+
+
     List<Rooms> findByCollegeCollegeId(Long collegeId);
+    Page<Rooms> findByCollegeCollegeId(Long collegeId, Pageable pageble);
+
 }
