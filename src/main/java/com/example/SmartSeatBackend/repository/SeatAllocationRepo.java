cefd21d76836f4eda8161f690c37a7b114384801
Gaurav Kumar Dixit <dixitgauravkumar69@gmail.com>
2026-02-23 13:36:57 +0530

    fix merge conflict of universty seating plan

--- src/main/java/com/example/SmartSeatBackend/repository/SeatAllocationRepo.java
+++ src/main/java/com/example/SmartSeatBackend/repository/SeatAllocationRepo.java
@@ -4,12 +4,7 @@ import com.example.SmartSeatBackend.entity.SeatAllocation;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SeatAllocationRepo extends JpaRepository< SeatAllocation,Long> {
-    package com.example.SmartSeatBackend.repository;
 
-import com.example.SmartSeatBackend.entity.SeatAllocation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
 
     public interface SeatAllocationRepo extends JpaRepository<SeatAllocation, Long> {
 
